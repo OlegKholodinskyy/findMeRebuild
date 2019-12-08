300af6b48b1d93845fe74db6dcc606691c793fb0
Oleg Kholodinskyi <ooleguner@gmail.com>
2019-12-08 12:48:48 +0200

    Addin User from page /users/new using separate parameters. Without Hibernate.

--- src/main/java/org/socialNetwork/controller/UserController.java
+++ src/main/java/org/socialNetwork/controller/UserController.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,11 +19,33 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    List<User> users = new ArrayList<>();
+
+
     @RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
     public String getUser(Model model, @PathVariable String userId) {
 
         User user = userService.getUser(userId);
         model.addAttribute("user", user);
         return "index";
+    }
+
+    @RequestMapping (path = "/users", method = RequestMethod.GET)
+    public String users(Model model){
+
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @RequestMapping(path = "/users/new", method = RequestMethod.GET)
+    public String newUser(){
+        return "newUser";
+    }
+
+    @RequestMapping(path = "/users/new", method = RequestMethod.POST)
+    public String addNewUser(@RequestParam("id") String id,
+                             @RequestParam("firstName") String firstName){
+        users.add(new User(Long.parseLong(id),firstName));
+        return "redirect:/users";
     }
 }
